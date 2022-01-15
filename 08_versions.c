16c636323fd2a0be1388258a257b234d83901b53
Miqueas <miqueas2020@yahoo.com>
2022-01-15 03:31:05 +0000

    Changed to macros

--- 08_versions.c
+++ 08_versions.c
@@ -5,14 +5,15 @@
 #include <lauxlib.h>
 
 int main(int argc, char **argv) {
-  if (LUA_VERSION_NUM == 501)
-    printf("Lua version: 5.1\n");
-  
-  if (LUA_VERSION_NUM == 502)
-    printf("Lua version: 5.2\n");
-  
-  if (LUA_VERSION_NUM == 503)
-    printf("Lua version: 5.3\n");
+  #if LUA_VERSION_NUM == 501
+  printf("Lua version: 5.1\n");
+  #elif LUA_VERSION_NUM == 502
+  printf("Lua version: 5.2\n");
+  #elif LUA_VERSION_NUM == 503
+  printf("Lua version: 5.3\n");
+  #elif LUA_VERSION_NUM == 504
+  printf("Lua version: 5.4\n");
+  #endif
 
   return 0;
 }
