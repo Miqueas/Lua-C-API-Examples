ce2feb2de46698308519d3e218da28613f6ee66e
JCPG <nounpatroon@gmail.com>
2023-01-16 22:35:25 -0500

    Add functions to userdata example

--- 06-userdata.c
+++ 06-userdata.c
@@ -5,12 +5,14 @@
 #include <lualib.h>
 #include <lauxlib.h>
 
-//#define str_arr_len(a) (sizeof(a) / sizeof(*a[0]))
+#define LIBBOOK_NAME "Book"
 
-/* In this example, we're pushing userdata to Lua
- */
+/*
+In this example, we're pushing userdata to Lua.
 
-// Our userdata to push, a simple "Book" object type
+Our userdata to push, a simple "Book" object type.
+*/
+
 typedef struct {
   char *title;
   char *author;
@@ -18,59 +20,61 @@ typedef struct {
   int   year;
 } Book;
 
-int book___index(lua_State *L);
-int book_new(lua_State *L);
+int book_sayhello(lua_State *L);
+
+int book__new(lua_State *L);
+
+int book_sayhello(lua_State *L) {
+  printf("\r\nHello world from book_sayhello()!\r\n");
+
+  return 0;
+}
 
 int luaopen_userdata(lua_State *L) {
-  lua_pushcfunction(L, book_new);
+  lua_register(L, LIBBOOK_NAME, book__new);
 
   return 1;
 }
 
-int book_new(lua_State *L) {
+int book__new(lua_State *L) {
   char *title  = (char*) luaL_checkstring(L, 1);
   char *author = (char*) luaL_checkstring(L, 2);
   int   pages  = luaL_checkint(L, 3);
   int   year   = luaL_checkint(L, 4);
 
-  // Cleans the stack
+  /*Cleans the stack*/
   lua_settop(L, 0);
 
-  // lua_newuserdata() is used like malloc()
+  /*lua_newuserdata() is used like malloc()*/
   Book *b = (Book *) lua_newuserdata(L, sizeof(Book));
   b->title  = title;
   b->author = author;
   b->pages  = pages;
   b->year   = year;
-  
-  // stack: Book(userdata), table
-  lua_newtable(L);
-  // stack: Book(userdata), table, "__index"
-  lua_pushstring(L, "__index");
-  // stack: Book(userdata), table, "__index", cfunction
-  lua_pushcfunction(L, book___index);
-  // stack: Book(userdata), table
-  lua_rawset(L, 2);
-  // stack: Book(userdata)
+
+  /*Add table to add variables and functions*/  
+  luaL_newmetatable(L, LIBBOOK_NAME);
+
+  /*Add index*/
+  lua_pushvalue(L, -1); lua_setfield(L, -2, "__index");
+
+  /*Add functions*/
+  /*
+  lua api functions commonly work with the top of the stack as arguments as well as output
+  */
+  lua_pushcfunction(L, book_sayhello); lua_setfield(L, -2, "sayhello");
+
+  /*Add variables*/
+  /*
+  lua api functions commonly work with the top of the stack as arguments as well as output
+  */
+  lua_pushstring(L, b->title); lua_setfield(L, -2, "title");
+  lua_pushstring(L, b->author); lua_setfield(L, -2, "author");
+  lua_pushinteger(L, b->pages); lua_setfield(L, -2, "pages");
+  lua_pushinteger(L, b->year); lua_setfield(L, -2, "year");
+
+  /*Add metatable to userdata*/
   lua_setmetatable(L, 1);
-
-  return 1;
-}
-
-int book___index(lua_State *L) {
-  Book *self = (Book *) lua_touserdata(L, 1);
-  const char *k = lua_tostring(L, 2);
-
-  if (!strcmp(k, "title"))
-    lua_pushstring(L, self->title);
-  else if (!strcmp(k, "author"))
-    lua_pushstring(L, self->author);
-  else if (!strcmp(k, "pages"))
-    lua_pushinteger(L, (lua_Integer) self->pages);
-  else if (!strcmp(k, "year"))
-    lua_pushinteger(L, (lua_Integer) self->year);
-  else
-    lua_pushnil(L);
 
   return 1;
 }
