bb7e53d60702bd592b717885adfae2295fa7e432
Miqueas <miqueas2020@yahoo.com>
2023-09-25 21:23:46 -0500

    Added hooks example

---
+++ src/11-hooks.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "constants.h"
+
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+void lineHook(lua_State *L, lua_Debug *ar) {
+  printf("Line execution!\n");
+  printf("Details:\n");
+
+  // Depending on what hook we handle, `ar` will be partially filled in some
+  // fields, so we need to manually full fill it
+  lua_getinfo(L, "S", ar);
+  lua_getinfo(L, "u", ar);
+
+  printf(" -> name: %s\n", ar->name);
+  printf(" -> namewhat: %s\n", ar->namewhat);
+  printf(" -> what: %s\n", ar->what);
+  printf(" -> source: %s\n", ar->source);
+  printf(" -> currentline: %d\n", ar->currentline);
+}
+
+int main(void) {
+  lua_State *vm = luaL_newstate();
+  lua_sethook(vm, lineHook, LUA_MASKLINE, 0);
+  int err = luaL_dofile(vm, HOOKS_PATH);
+  printf("Code: %d\n", err);
+  lua_close(vm);
+
+  return 0;
+}
---
+++ src/11-hooks.lua
@@ -0,0 +1,7 @@
+local name = "Carlos"
+local age = 29
+
+print(name, age)
+
+age = 23
+name = "Josue"
